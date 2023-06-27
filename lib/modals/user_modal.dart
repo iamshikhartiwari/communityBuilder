2ecc2126ba79f9ed47056a371c77d2687e6c8a77
Shikhar <iamshikhartiwari.com>
2023-06-27 11:29:35 +0530

    internal commit

--- lib/modals/user_modal.dart
+++ lib/modals/user_modal.dart
@@ -4,7 +4,7 @@ class UserModel {
   final String name;
   final String profilePic;
   final String banner;
-  final String uid;
+  final String uid; 
   final bool isAuthenticated; // if guest or not
   final int karma;
   final List<String> awards;
